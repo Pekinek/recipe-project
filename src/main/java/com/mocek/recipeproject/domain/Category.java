908a742f3cf94d9b584e713cdb0282976d91916b
pekinek <pekinhere@gmail.com>
2018-09-20 23:09:34 +0200

    project lombok added

--- src/main/java/com/mocek/recipeproject/domain/Category.java
+++ src/main/java/com/mocek/recipeproject/domain/Category.java
@@ -1,9 +1,14 @@
 package com.mocek.recipeproject.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@EqualsAndHashCode(exclude = {"recipes"})
+@Data
 public class Category {
 
     @Id
@@ -14,27 +19,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
